03aad1e712ddda80f0353b9f2de0dc3a8407a416
filiz <filiz@DESKTOP-PDJR58L>
2025-12-18 21:10:34 +0300

    Add project files.

---
+++ Petshop_Projesi_Otomasyon/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Petshop_Projesi_Otomasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMusteriIslemleri_Click(object sender, EventArgs e)
+        {
+            FrmMusteri frm = new FrmMusteri();
+            frm.Show();
+            this.Hide();
+        }
+    }
+
+}
---
+++ Petshop_Projesi_Otomasyon/FrmMusteri.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Petshop_Projesi_Otomasyon
+{
+    partial class FrmMusteri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdsoyad = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.btnMusteriListele = new System.Windows.Forms.Button();
+            this.btnMusteriEkle = new System.Windows.Forms.Button();
+            this.btnMusteriSil = new System.Windows.Forms.Button();
+            this.btnMusteriGuncelle = new System.Windows.Forms.Button();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefon Numarası";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adres";
+            // 
+            // txtAdsoyad
+            // 
+            this.txtAdsoyad.Location = new System.Drawing.Point(238, 222);
+            this.txtAdsoyad.Name = "txtAdsoyad";
+            this.txtAdsoyad.Size = new System.Drawing.Size(100, 22);
+            this.txtAdsoyad.TabIndex = 3;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(238, 272);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(100, 22);
+            this.txtTelefon.TabIndex = 4;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(238, 326);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(100, 22);
+            this.txtAdres.TabIndex = 5;
+            // 
+            // btnMusteriListele
+            // 
+            this.btnMusteriListele.Location = new System.Drawing.Point(218, 391);
+            this.btnMusteriListele.Name = "btnMusteriListele";
+            this.btnMusteriListele.Size = new System.Drawing.Size(75, 23);
+            this.btnMusteriListele.TabIndex = 6;
+            this.btnMusteriListele.Text = "Listele";
+            this.btnMusteriListele.UseVisualStyleBackColor = true;
+            // 
+            // btnMusteriEkle
+            // 
+            this.btnMusteriEkle.Location = new System.Drawing.Point(91, 391);
+            this.btnMusteriEkle.Name = "btnMusteriEkle";
+            this.btnMusteriEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnMusteriEkle.TabIndex = 7;
+            this.btnMusteriEkle.Text = "Ekle";
+            this.btnMusteriEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnMusteriSil
+            // 
+            this.btnMusteriSil.Location = new System.Drawing.Point(495, 391);
+            this.btnMusteriSil.Name = "btnMusteriSil";
+            this.btnMusteriSil.Size = new System.Drawing.Size(75, 23);
+            this.btnMusteriSil.TabIndex = 8;
+            this.btnMusteriSil.Text = "Sil";
+            this.btnMusteriSil.UseVisualStyleBackColor = true;
+            // 
+            // btnMusteriGuncelle
+            // 
+            this.btnMusteriGuncelle.Location = new System.Drawing.Point(368, 391);
+            this.btnMusteriGuncelle.Name = "btnMusteriGuncelle";
+            this.btnMusteriGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnMusteriGuncelle.TabIndex = 9;
+            this.btnMusteriGuncelle.Text = "Güncelle";
+            this.btnMusteriGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(608, 391);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(75, 23);
+            this.btnGeri.TabIndex = 10;
+            this.btnGeri.Text = "Menü";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            // 
+            // FrmMusteri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGeri);
+            this.Controls.Add(this.btnMusteriGuncelle);
+            this.Controls.Add(this.btnMusteriSil);
+            this.Controls.Add(this.btnMusteriEkle);
+            this.Controls.Add(this.btnMusteriListele);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtAdsoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMusteri";
+            this.Text = "FrmMusteri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdsoyad;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Button btnMusteriListele;
+        private System.Windows.Forms.Button btnMusteriEkle;
+        private System.Windows.Forms.Button btnMusteriSil;
+        private System.Windows.Forms.Button btnMusteriGuncelle;
+        private System.Windows.Forms.Button btnGeri;
+    }
+}
